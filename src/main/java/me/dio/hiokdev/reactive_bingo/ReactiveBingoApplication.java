e68c2a8cef5d12b7f73cfaaa786b826f0805ec0d
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-05-24 14:37:53 -0300

    feat: generate spring project with spring initializr

---
+++ src/main/java/me/dio/hiokdev/reactive_bingo/ReactiveBingoApplication.java
@@ -0,0 +1,13 @@
+package me.dio.hiokdev.reactive_bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveBingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveBingoApplication.class, args);
+	}
+
+}
