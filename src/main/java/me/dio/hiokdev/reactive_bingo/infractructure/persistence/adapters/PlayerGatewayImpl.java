f4edba72709882197e95647a6d90ad46717005b2
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-05-28 22:49:59 -0300

    feat: create persistence gateways impls

---
+++ src/main/java/me/dio/hiokdev/reactive_bingo/infractructure/persistence/adapters/PlayerGatewayImpl.java
@@ -0,0 +1,51 @@
+package me.dio.hiokdev.reactive_bingo.infractructure.persistence.adapters;
+
+import lombok.RequiredArgsConstructor;
+import me.dio.hiokdev.reactive_bingo.domain.dto.PageablePlayers;
+import me.dio.hiokdev.reactive_bingo.domain.dto.PagedPlayers;
+import me.dio.hiokdev.reactive_bingo.domain.gateways.PlayerGateway;
+import me.dio.hiokdev.reactive_bingo.domain.models.Player;
+import me.dio.hiokdev.reactive_bingo.infractructure.persistence.mappers.PlayerDocumentMapper;
+import me.dio.hiokdev.reactive_bingo.infractructure.persistence.repositories.PlayerDocumentRepository;
+import me.dio.hiokdev.reactive_bingo.infractructure.persistence.repositories.impl.FindOnDemandPlayerRepositoryImpl;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class PlayerGatewayImpl implements PlayerGateway {
+
+    private final PlayerDocumentRepository playerDocumentRepository;
+    private final FindOnDemandPlayerRepositoryImpl findOnDemandPlayerRepository;
+    private final PlayerDocumentMapper playerDocumentMapper;
+
+    @Override
+    public Mono<Player> save(Player player) {
+        return playerDocumentRepository.save(playerDocumentMapper.toDocument(player))
+                .map(playerDocumentMapper::toDomainModel);
+    }
+
+    @Override
+    public Mono<PagedPlayers> findOnDemand(PageablePlayers pageable) {
+        return findOnDemandPlayerRepository.findOnDemand(pageable)
+                .collectList()
+                .zipWhen(documents -> findOnDemandPlayerRepository.count(pageable))
+                .map(tuple -> PagedPlayers.builder()
+                        .currentPage(pageable.page())
+                        .totalPages((tuple.getT2() / pageable.limit())
+                                + (((tuple.getT2()) % pageable.limit() > 0) ? 1 : 0))
+                        .totalItens(tuple.getT2())
+                        .content(tuple.getT1().stream().map(playerDocumentMapper::toDomainModel).toList())
+                        .build());
+    }
+
+    @Override
+    public Mono<Player> findById(String id) {
+        return playerDocumentRepository.findById(id)
+                .map(playerDocumentMapper::toDomainModel);
+    }
+
+    @Override
+    public Mono<Void> delete(Player player) {
+        return playerDocumentRepository.delete(playerDocumentMapper.toDocument(player));
+    }
+
+}
---
+++ src/main/java/me/dio/hiokdev/reactive_bingo/infractructure/persistence/adapters/RoundGatewayImpl.java
@@ -0,0 +1,58 @@
+package me.dio.hiokdev.reactive_bingo.infractructure.persistence.adapters;
+
+import lombok.RequiredArgsConstructor;
+import me.dio.hiokdev.reactive_bingo.domain.dto.PageableRounds;
+import me.dio.hiokdev.reactive_bingo.domain.dto.PagedRounds;
+import me.dio.hiokdev.reactive_bingo.domain.gateways.RoundGateway;
+import me.dio.hiokdev.reactive_bingo.domain.models.Round;
+import me.dio.hiokdev.reactive_bingo.infractructure.persistence.mappers.RoundDocumentMapper;
+import me.dio.hiokdev.reactive_bingo.infractructure.persistence.repositories.RoundDocumentRepository;
+import me.dio.hiokdev.reactive_bingo.infractructure.persistence.repositories.impl.FindOnDemandRoundRepositoryImpl;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public class RoundGatewayImpl implements RoundGateway {
+
+    private final RoundDocumentRepository roundDocumentRepository;
+    private final FindOnDemandRoundRepositoryImpl findOnDemandRoundRepository;
+    private final RoundDocumentMapper roundDocumentMapper;
+
+    @Override
+    public Mono<Round> save(Round round) {
+        return roundDocumentRepository.save(roundDocumentMapper.toDocument(round))
+                .map(roundDocumentMapper::toDomainModel);
+    }
+
+    @Override
+    public Flux<Round> findAll() {
+        return roundDocumentRepository.findAll()
+                .map(roundDocumentMapper::toDomainModel);
+    }
+
+    @Override
+    public Mono<PagedRounds> findOnDemand(PageableRounds pageable) {
+        return findOnDemandRoundRepository.findOnDemand(pageable)
+                .collectList()
+                .zipWhen(documents -> findOnDemandRoundRepository.count(pageable))
+                .map(tuple -> PagedRounds.builder()
+                        .currentPage(pageable.page())
+                        .totalPages((tuple.getT2() / pageable.limit())
+                                + (((tuple.getT2()) % pageable.limit() > 0) ? 1 : 0))
+                        .totalItens(tuple.getT2())
+                        .content(tuple.getT1().stream().map(roundDocumentMapper::toDomainModel).toList())
+                        .build());
+    }
+
+    @Override
+    public Mono<Round> findById(String id) {
+        return roundDocumentRepository.findById(id)
+                .map(roundDocumentMapper::toDomainModel);
+    }
+
+    @Override
+    public Mono<Boolean> existsByIdAndPlayerId(String id, String playerId) {
+        return roundDocumentRepository.existsByIdAndBingoCards_Player_Id(id, playerId);
+    }
+
+}
