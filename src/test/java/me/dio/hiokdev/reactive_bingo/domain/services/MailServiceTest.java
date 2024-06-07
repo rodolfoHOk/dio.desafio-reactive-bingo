ae20ec4ab63443a427739ba527aa13fd220db897
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-06-07 16:53:51 -0300

    test: create tests setup for mail service

--- src/test/java/me/dio/hiokdev/reactive_bingo/domain/services/MailServiceTest.java
+++ src/test/java/me/dio/hiokdev/reactive_bingo/domain/services/MailServiceTest.java
@@ -1,4 +1,110 @@
 package me.dio.hiokdev.reactive_bingo.domain.services;
 
+import com.github.javafaker.Faker;
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
+import me.dio.hiokdev.reactive_bingo.core.factory.FakerData;
+import me.dio.hiokdev.reactive_bingo.core.retry.RetryConfig;
+import me.dio.hiokdev.reactive_bingo.core.retry.RetryHelper;
+import me.dio.hiokdev.reactive_bingo.domain.gateways.MailGateway;
+import me.dio.hiokdev.reactive_bingo.infractructure.mail.adapters.MailGatewayImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.mail.MailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.util.Properties;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
 public class MailServiceTest {
+
+    private static final Integer PORT = 80;
+    private static final String USER = "teste@teste.com.br";
+    private static final String PASSWORD = "123456";
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    private GreenMail smtpServer;
+    private MailService mailService;
+    private final Faker faker = FakerData.getFaker();
+
+    @BeforeEach
+    void setup() {
+        this.smtpServer = createSmtpServer(PORT, USER, PASSWORD);
+        String sender = faker.internet().emailAddress();
+        var host = smtpServer.getSmtp().getServerSetup().getBindAddress();
+        JavaMailSender mailSender = createMailSender(host, PORT, USER, PASSWORD);
+        TemplateEngine templateEngine = createTemplateEngine(applicationContext);
+        RetryHelper retryHelper = new RetryHelper(new RetryConfig(1L, 1L));
+        MailGateway mailGateway = new MailGatewayImpl(sender, mailSender, templateEngine, retryHelper);
+        this.mailService = new MailService(mailGateway);
+        this.smtpServer.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.smtpServer.stop();
+    }
+
+    @Test
+    void whenSendToWinnerThenReturnVoid() {
+        MailMessage winnerMailMessage = null;
+        // TODO
+    }
+
+    @Test
+    void whenSendToNonWinnerThenReturnVoid() {
+        MailMessage mailMessage = null;
+        // TODO
+    }
+
+    private GreenMail createSmtpServer(Integer port, String user, String password) {
+        var smtpServer = new GreenMail(new ServerSetup(port, null, "smtp"));
+        smtpServer.setUser(user, password);
+        return smtpServer;
+    }
+
+    private JavaMailSenderImpl createMailSender(String host, Integer port, String user, String password) {
+        var sender = new JavaMailSenderImpl();
+        sender.setHost(host);
+        sender.setPort(port);
+        var mailProperties = new Properties();
+        mailProperties.setProperty("mail.transport.protocol", "smtp");
+        mailProperties.setProperty("mail.smtp.auth", "true");
+        mailProperties.setProperty("mail.smtp.starttls.enable", "true");
+        mailProperties.setProperty("mail.debug", "false");
+        sender.setJavaMailProperties(mailProperties);
+        sender.setUsername(user);
+        sender.setPassword(password);
+        return sender;
+    }
+
+    private SpringTemplateEngine createTemplateEngine(ApplicationContext applicationContext) {
+        var templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setApplicationContext(applicationContext);
+
+        var templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        templateEngine.setEnableSpringELCompiler(true);
+        templateEngine.addDialect(new Java8TimeDialect());
+        return templateEngine;
+    }
+
 }
